d8a091072043224a79e3ce544f5a6ed0ca36e1aa
Dennis Idowu <dennis_idowu@live.com>
2020-11-10 20:39:29 -0500

    restructed app.js and changed from funcitonal component to class based component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Welcome to COVID-Tracker</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1> Welcome to COVID-19 Tracker</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
