4952dfb4878b78a009deac0651b64fbbec662a9b
Dennis Idowu <dennis_idowu@live.com>
2020-11-17 16:23:12 -0500

    populated the countries on the application for the picker

--- front-end/src/components/CountryPicker/CountryPicker.jsx
+++ front-end/src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {NativeSelect, FormControl} from '@material-ui/core';
-import {fetchCountries} from '../../api/index';
+import {fetchCountries} from '../../api';
 import styles from './CountryPicker.module.css';
 
 const CountryPicker = () =>{
-    const [fetchAPI, setFetchedCountries] = useState([]);
+    const [fetchedCountries, setFetchedCountries] = useState([]);
     useEffect(() => {
        const fetchAPI = async() =>{
            setFetchedCountries(await fetchCountries())
@@ -15,6 +15,7 @@ const CountryPicker = () =>{
         <FormControl className={styles.formControl}>
             <NativeSelect>
                 <option value="global">Global</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
