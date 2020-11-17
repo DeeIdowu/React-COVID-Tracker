f7e226e68401cca454a5e568b7844a8f9e42928a
Dennis Idowu <dennis_idowu@live.com>
2020-11-17 16:11:11 -0500

    applied usestate and useeffect to fetch countries from api and display them

--- front-end/src/components/CountryPicker/CountryPicker.jsx
+++ front-end/src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import {NativeSelect, FormControl} from '@material-ui/core';
+import {countries} from '../../api/index';
 import styles from './CountryPicker.module.css';
 
 const CountryPicker = () =>{
+    const [fetchCountries, setFetchedCountries] = useState([]);
+    useEffect(() => {
+       const fetchCountries = async() =>{
+           setFetchedCountries(await countries)
+       }
+       fetchCountries();
+    })
     return(
         <FormControl className={styles.formControl}>
             <NativeSelect>
