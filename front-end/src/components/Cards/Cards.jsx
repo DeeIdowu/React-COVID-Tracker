d199fa67678ea4119dede664b22fda27032c16d9
Dennis Idowu <dennis_idowu@live.com>
2020-11-18 22:52:57 -0500

    fixed the styling of cards to make mobile responsive

--- front-end/src/components/Cards/Cards.jsx
+++ front-end/src/components/Cards/Cards.jsx
@@ -14,70 +14,57 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate} }) =>{
 
     return(
         <div className={styles.container}>
-          <Grid spacing={3} justify="center">
-            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                        Infected
-                    </Typography>
-                     <Typography variant="body2">
-                        No. of Active COVID-19 Cases:
-                    </Typography>
-                    <Typography variant="h5">
-                        <CountUp
-                            start={0}
-                            end={confirmed.value}
-                            duration={2.5}
-                            seperator=","/>
-                    </Typography>
-                    <Typography color="textSecondary">
-                       {new Date(lastUpdate).toDateString()}
-                    </Typography>
-                </CardContent>
-            </Grid>
-            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                        Recovered
-                    </Typography>
-                     <Typography variant="body2">
-                        No. of Recoveries from COVID-19:
-                    </Typography>
-                   <Typography variant="h5">
-                        <CountUp
-                            start={0}
-                            end={recovered.value}
-                            duration={2.5}
-                            seperator=","/>
-                    </Typography>
-                   <Typography color="textSecondary">
-                       {new Date(lastUpdate).toDateString()}
-                    </Typography>
-                   
-                </CardContent>
-            </Grid>
-            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                        Deaths
-                    </Typography>
-                    <Typography variant="body2">
-                        No. of Deaths caused by COVID-19:
-                    </Typography>
-                    <Typography variant="h5">
-                        <CountUp
-                            start={0}
-                            end={deaths.value}
-                            duration={2.5}
-                            seperator=","/>
-                    </Typography>
-                    <Typography color="textSecondary">
-                     {new Date(lastUpdate).toDateString()}
-                    </Typography>
-                </CardContent>
-            </Grid>
-          </Grid>
-        </div>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              No. of active cases of COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={recovered.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              No. of recoveries from COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={deaths.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p">
+              No. of deaths caused by COVID-19.
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
     )
 }
 
